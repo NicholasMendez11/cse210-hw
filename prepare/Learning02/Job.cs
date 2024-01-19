e1fb39e4e31d050374e1ae2b8be202ed771872da
axploy <nicholasmeeendez@gmail.com>
2024-01-19 09:25:24 -0500

    Add resume display functionality
    Add resume display functionality

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+public class Job
+{
+    public string Company { get; set; }
+    public string JobTitle { get; set; }
+    public int StartYear { get; set; }
+    public int EndYear { get; set; }
+
+    public void Display()
+    {
+        Console.WriteLine($"{JobTitle} ({Company}) {StartYear}-{EndYear}");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        var job1 = new Job { Company = "Microsoft", JobTitle = "Software Engineer", StartYear = 2019, EndYear = 2022 };
+        var job2 = new Job { Company = "Apple", JobTitle = "Manager", StartYear = 2022, EndYear = 2023 };
+
+        var resume = new Resume { Name = "Nicholas Mendez" };
+        resume.Jobs.Add(job1);
+        resume.Jobs.Add(job2);
+
+        resume.Display();
     }
 }
